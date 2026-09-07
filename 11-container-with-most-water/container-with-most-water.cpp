e33fa38e938da8e5700497e0ff39e24160680adc
Syntax_surfer <ayushmuz88@gmail.com>
2026-09-07 22:27:19 +0530

    Time: 0 ms (100.00%) | Memory: 62.9 MB (48.90%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        //tle code brute force 
+        //O(n)
+        int n =  height.size();
+        // int max_water = 0; //ans
+        // for(int i =0 ; i<n;i++)
+        // {
+        //     for(int j = i+1;j<n;j++)
+        //     {
+        //         int w = j-i;//w = water
+        //         int ht = min(height[i],height[j]); //ht = height
+        //         int current_water = w * ht;
+                
+        //         max_water = max(max_water,current_water);
+        //     }
+        // }
+        // return max_water;
+
+        //two pointer approach
+          int ans =0;
+          int lp = 0;
+          int max_water = 0; // ans
+          int rp = n-1;
+          while(lp<rp)
+          {
+            int w = rp - lp;
+            int ht = min(height[lp],height[rp]);
+            int cw  = w*ht;//current water
+            max_water = max(max_water,cw);
+            height[lp]<height[rp] ? lp++ :rp--;
+          }
+          return max_water;
+
+    }
+};
